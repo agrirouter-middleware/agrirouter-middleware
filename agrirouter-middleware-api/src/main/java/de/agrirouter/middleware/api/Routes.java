44e8e4ccabc4fd34c1ea966b94e35625937c7e9f
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2022-02-10 09:57:53 +0100

    Bugfix/fix thymeleaf template resolving (#18)
    * Add Swagger UI to GitHub pages.
    
    * Adapt route declaration.
    
    * Added settings for external CI process.
    
    * Added settings for external CI process.
    
    * Adapt POM to match current, external configuration

--- agrirouter-middleware-api/src/main/java/de/agrirouter/middleware/api/Routes.java
+++ agrirouter-middleware-api/src/main/java/de/agrirouter/middleware/api/Routes.java
@@ -4,6 +4,6 @@ package de.agrirouter.middleware.api;
  * Central routing definitions.
  */
 public class Routes {
-    public static final String ONBOARD_PROCESS_RESULT = "/onboard-process-result";
-    public static final String HOME = "/home";
+    public static final String ONBOARD_PROCESS_RESULT = "onboard-process-result";
+    public static final String HOME = "home";
 }
