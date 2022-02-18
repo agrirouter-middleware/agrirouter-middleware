3eaab92a321d6a0c6d2dedbc96a7d67e48467c26
Oliver Rahner <o.rahner@dke-data.com>
2022-02-18 18:22:15 +0100

    fix filename...

---
+++ mongo-init.sh
@@ -0,0 +1,14 @@
+set -e
+
+mongo <<EOF
+use $MONGO_INITDB_DATABASE
+
+db.createUser({
+  user: '$MONGO_INITDB_USER',
+  pwd: '$MONGO_INITDB_PWD',
+  roles: [{
+    role: 'readWrite',
+    db: '$MONGO_INITDB_DATABASE'
+  }]
+})
+EOF
