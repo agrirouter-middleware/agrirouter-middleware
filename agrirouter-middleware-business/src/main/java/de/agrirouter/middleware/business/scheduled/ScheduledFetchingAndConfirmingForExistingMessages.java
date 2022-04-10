dfd75907ec527d87030eddaf765e7f54de45764b
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2022-04-10 19:48:00 +0200

    Log scheduled messages fetching.

--- agrirouter-middleware-business/src/main/java/de/agrirouter/middleware/business/scheduled/ScheduledFetchingAndConfirmingForExistingMessages.java
+++ agrirouter-middleware-business/src/main/java/de/agrirouter/middleware/business/scheduled/ScheduledFetchingAndConfirmingForExistingMessages.java
@@ -6,13 +6,14 @@ import com.dke.data.agrirouter.api.service.parameters.MessageQueryParameters;
 import com.dke.data.agrirouter.impl.messaging.mqtt.MessageQueryServiceImpl;
 import de.agrirouter.middleware.api.errorhandling.BusinessException;
 import de.agrirouter.middleware.api.errorhandling.error.ErrorMessageFactory;
+import de.agrirouter.middleware.api.logging.BusinessOperationLogService;
+import de.agrirouter.middleware.api.logging.EndpointLogInformation;
 import de.agrirouter.middleware.domain.Endpoint;
 import de.agrirouter.middleware.integration.ack.MessageWaitingForAcknowledgement;
 import de.agrirouter.middleware.integration.ack.MessageWaitingForAcknowledgementService;
 import de.agrirouter.middleware.integration.mqtt.MqttClientManagementService;
 import de.agrirouter.middleware.persistence.EndpointRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -22,21 +23,23 @@ import java.time.temporal.ChronoUnit;
 /**
  * Service for endpoint maintenance.
  */
+@Slf4j
 @Service
 public class ScheduledFetchingAndConfirmingForExistingMessages {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledFetchingAndConfirmingForExistingMessages.class);
 
     private final EndpointRepository endpointRepository;
     private final MessageWaitingForAcknowledgementService messageWaitingForAcknowledgementService;
     private final MqttClientManagementService mqttClientManagementService;
+    private final BusinessOperationLogService businessOperationLogService;
 
     public ScheduledFetchingAndConfirmingForExistingMessages(EndpointRepository endpointRepository,
                                                              MessageWaitingForAcknowledgementService messageWaitingForAcknowledgementService,
-                                                             MqttClientManagementService mqttClientManagementService) {
+                                                             MqttClientManagementService mqttClientManagementService,
+                                                             BusinessOperationLogService businessOperationLogService) {
         this.endpointRepository = endpointRepository;
         this.mqttClientManagementService = mqttClientManagementService;
         this.messageWaitingForAcknowledgementService = messageWaitingForAcknowledgementService;
+        this.businessOperationLogService = businessOperationLogService;
     }
 
     /**
@@ -44,12 +47,15 @@ public class ScheduledFetchingAndConfirmingForExistingMessages {
      */
     @Scheduled(cron = "${app.scheduled.fetching-and-confirming-existing-messages}")
     public void scheduleFetchingAndConfirmingExistingMessagesForAllEndpoints() {
-        LOGGER.debug("Scheduled fetching and confirming for existing messages.");
-        endpointRepository.findAll().stream().filter(endpoint -> !endpoint.isDeactivated()).forEach(this::fetchAndConfirmExistingMessages);
+        log.debug("Scheduled fetching and confirming for existing messages.");
+        endpointRepository.findAll().stream().filter(endpoint -> !endpoint.isDeactivated()).forEach(endpoint -> {
+            this.fetchAndConfirmExistingMessages(endpoint);
+            businessOperationLogService.log(new EndpointLogInformation(endpoint.getExternalEndpointId(), endpoint.getAgrirouterEndpointId()), "Scheduled fetching and confirming of existing messages.");
+        });
     }
 
     private void fetchAndConfirmExistingMessages(Endpoint endpoint) {
-        LOGGER.debug("Fetching and confirming existing messages for endpoint '{}'.", endpoint.getExternalEndpointId());
+        log.debug("Fetching and confirming existing messages for endpoint '{}'.", endpoint.getExternalEndpointId());
         final var iMqttClient = mqttClientManagementService.get(endpoint.asOnboardingResponse());
         if (iMqttClient.isEmpty()) {
             throw new BusinessException(ErrorMessageFactory.couldNotConnectMqttClient(endpoint.asOnboardingResponse().getSensorAlternateId()));
@@ -61,7 +67,7 @@ public class ScheduledFetchingAndConfirmingForExistingMessages {
         parameters.setOnboardingResponse(endpoint.asOnboardingResponse());
         final var messageId = messageQueryService.send(parameters);
 
-        LOGGER.debug("Saving message with ID '{}'  waiting for ACK.", messageId);
+        log.debug("Saving message with ID '{}'  waiting for ACK.", messageId);
         MessageWaitingForAcknowledgement messageWaitingForAcknowledgement = new MessageWaitingForAcknowledgement();
         messageWaitingForAcknowledgement.setAgrirouterEndpointId(endpoint.getAgrirouterEndpointId());
         messageWaitingForAcknowledgement.setMessageId(messageId);
