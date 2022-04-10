fa3f5fec54e706a555d86c355a009b38f5026640
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2022-04-10 19:20:27 +0200

    Log reconnect events for the endpoints.

--- agrirouter-middleware-business/src/main/java/de/agrirouter/middleware/business/listener/ReconnectAllOnboardResponsesEventListener.java
+++ agrirouter-middleware-business/src/main/java/de/agrirouter/middleware/business/listener/ReconnectAllOnboardResponsesEventListener.java
@@ -5,20 +5,18 @@ import de.agrirouter.middleware.api.errorhandling.error.ErrorMessageFactory;
 import de.agrirouter.middleware.api.events.EndpointStatusUpdateEvent;
 import de.agrirouter.middleware.integration.mqtt.MqttClientManagementService;
 import de.agrirouter.middleware.persistence.EndpointRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 /**
- *
+ * Reconnect all existing onboard responses (if necessary).
  */
+@Slf4j
 @Component
 public class ReconnectAllOnboardResponsesEventListener {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ReconnectAllOnboardResponsesEventListener.class);
 
     private final EndpointRepository endpointRepository;
     private final MqttClientManagementService mqttClientManagementService;
@@ -39,7 +37,7 @@ public class ReconnectAllOnboardResponsesEventListener {
      */
     @EventListener
     public void reconnectAllOnboardResponses(ApplicationReadyEvent applicationReadyEvent) {
-        LOGGER.debug("The application is ready.");
+        log.debug("The application is ready.");
         endpointRepository.findAll().stream().filter(endpoint -> !endpoint.isDeactivated()).forEach(endpoint -> {
             try {
                 final var iMqttClient = mqttClientManagementService.get(endpoint.asOnboardingResponse());
@@ -47,12 +45,13 @@ public class ReconnectAllOnboardResponsesEventListener {
                     throw new BusinessException(ErrorMessageFactory.couldNotConnectMqttClient(endpoint.asOnboardingResponse().getSensorAlternateId()));
                 }
                 final var onboardingResponse = endpoint.asOnboardingResponse();
-                LOGGER.debug("Connect MQTT client for endpoint with the ID '{}' and client ID '{}'.", onboardingResponse.getSensorAlternateId(), onboardingResponse.getConnectionCriteria().getClientId());
+                log.debug("Connect MQTT client for endpoint with the ID '{}' and client ID '{}'.", onboardingResponse.getSensorAlternateId(), onboardingResponse.getConnectionCriteria().getClientId());
                 final var mqttClient = mqttClientManagementService.get(onboardingResponse);
-                LOGGER.debug("MQTT client for endpoint with the ID '{}' and client ID '{}' connected", onboardingResponse.getSensorAlternateId(), iMqttClient.get().getClientId());
+                mqttClient.ifPresentOrElse(mc -> log.debug("MQTT client for endpoint with the ID '{}' and client ID '{}' connected", onboardingResponse.getSensorAlternateId(), mc.getClientId()), () ->
+                        log.error("Could not reconnect a client, please check the client to avoid data loss."));
                 applicationEventPublisher.publishEvent(new EndpointStatusUpdateEvent(this, endpoint.getAgrirouterEndpointId(), null));
             } catch (Exception e) {
-                LOGGER.error("Could not reconnect a client, please check the client to avoid data loss.", e);
+                log.error("Could not reconnect a client, please check the client to avoid data loss.", e);
             }
         });
     }
