d37aef105be8298b4f6707d85498710a750cb570
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2022-07-22 07:16:40 +0200

    Feature/add custom team set context id for machine (#95)
    * Remove cycle from internal dependencies.
    
    * Add a custom team set context ID to the request in case the platform wants to define it own their own.

--- agrirouter-middleware-controller/src/main/java/de/agrirouter/middleware/controller/dto/request/RegisterMachineRequest.java
+++ agrirouter-middleware-controller/src/main/java/de/agrirouter/middleware/controller/dto/request/RegisterMachineRequest.java
@@ -17,6 +17,9 @@ import javax.validation.constraints.NotNull;
 @Schema(description = "A request to register a machine.")
 public class RegisterMachineRequest {
 
+    @Schema(description = "Your custom team set context ID, in case you want to define it by your own. If the field is empty, a unique ID will be generated.")
+    private String customTeamSetContextId;
+
     /**
      * The device description itself, should be Base64 encoded.
      */
