35cc26b4628eba34b5916f30c3a6d257fb650651
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2022-09-02 15:56:07 +0200

    Add connection errors to the dashboard. Fix typos. Fix button text.

--- agrirouter-middleware-integration/src/main/java/de/agrirouter/middleware/integration/mqtt/ConnectionError.java
+++ agrirouter-middleware-integration/src/main/java/de/agrirouter/middleware/integration/mqtt/ConnectionError.java
@@ -5,8 +5,9 @@ import java.time.Instant;
 /**
  * A single connection error.
  *
- * @param pointInTime  The point in time the connection error occured.
+ * @param pointInTime  The point in time the connection error occurred.
  * @param errorMessage The error message.
  */
+@SuppressWarnings("unused")
 public record ConnectionError(Instant pointInTime, String errorMessage) {
 }
