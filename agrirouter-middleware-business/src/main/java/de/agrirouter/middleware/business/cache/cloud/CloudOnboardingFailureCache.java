89df4b77fe1a9d74f81d4fbeda43e0676dcedd33
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2022-09-07 09:19:07 +0200

    Add cloud onboarding errors to the cache.

--- agrirouter-middleware-business/src/main/java/de/agrirouter/middleware/business/cache/cloud/CloudOnboardingFailureCache.java
+++ agrirouter-middleware-business/src/main/java/de/agrirouter/middleware/business/cache/cloud/CloudOnboardingFailureCache.java
@@ -3,7 +3,9 @@ package de.agrirouter.middleware.business.cache.cloud;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Cache for cloud onboarding failures.
@@ -23,19 +25,28 @@ public class CloudOnboardingFailureCache {
      * @param errorMessage              The error message.
      */
     public void put(String externalEndpointId, String virtualExternalEndpointId, String errorCode, String errorMessage) {
-        cache.put(externalEndpointId, new FailureEntry(externalEndpointId, virtualExternalEndpointId, errorCode, errorMessage));
+        cache.put(virtualExternalEndpointId, new FailureEntry(Instant.now(), externalEndpointId, virtualExternalEndpointId, errorCode, errorMessage));
+    }
+
+    /**
+     * Get all entries for the external endpoint ID.
+     */
+    public List<FailureEntry> get(String externalEndpointId) {
+        return cache.values().stream().filter(failureEntry -> failureEntry.externalEndpointId.equals(externalEndpointId)).toList();
     }
 
     /**
      * The cache entry.
      *
+     * @param timestamp                 The timestamp.
      * @param externalEndpointId        The external endpoint ID.
      * @param virtualExternalEndpointId The virtual external endpoint ID.
      * @param errorCode                 The error code.
      * @param errorMessage              The error message.
      */
-    private record FailureEntry(String externalEndpointId, String virtualExternalEndpointId, String errorCode,
-                                String errorMessage) {
+    public record FailureEntry(Instant timestamp, String externalEndpointId, String virtualExternalEndpointId,
+                               String errorCode,
+                               String errorMessage) {
     }
 
 }
