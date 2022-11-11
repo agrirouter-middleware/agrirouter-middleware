550ef98c65d41f99f73480f5f2ac2f232565f73e
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2022-11-11 19:23:42 +0100

    Feature/format timestamps in UI (#154)
    * Format timestamps in the UI.
    Sort messages waiting for ACK.
    
    * Adapt CI workflow to run on pushes against main as well.
    
    * Move / refactor docker compose files.

--- agrirouter-middleware-application/src/main/java/de/agrirouter/middleware/controller/EndpointDashboardUIController.java
+++ agrirouter-middleware-application/src/main/java/de/agrirouter/middleware/controller/EndpointDashboardUIController.java
@@ -5,13 +5,8 @@ import de.agrirouter.middleware.business.ApplicationService;
 import de.agrirouter.middleware.business.EndpointService;
 import de.agrirouter.middleware.business.cache.cloud.CloudOnboardingFailureCache;
 import de.agrirouter.middleware.controller.dto.response.domain.MessageWaitingForAcknowledgementDto;
-import de.agrirouter.middleware.domain.Application;
-import de.agrirouter.middleware.domain.Endpoint;
-import de.agrirouter.middleware.domain.log.Error;
-import de.agrirouter.middleware.domain.log.Warning;
 import de.agrirouter.middleware.integration.ack.MessageWaitingForAcknowledgementService;
 import de.agrirouter.middleware.integration.mqtt.MqttClientManagementService;
-import de.agrirouter.middleware.integration.mqtt.TechnicalConnectionState;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,9 +15,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * The custom home controller.
@@ -59,35 +53,36 @@ public class EndpointDashboardUIController {
     @SuppressWarnings("unused")
     @GetMapping("/endpoint-dashboard")
     public String navigation(Principal principal, @RequestParam(value = "externalEndpointId") String externalEndpointId, Model model) {
-        Optional<Endpoint> optionalEndpoint = endpointService.findByExternalEndpointId(externalEndpointId);
+        var optionalEndpoint = endpointService.findByExternalEndpointId(externalEndpointId);
         if (optionalEndpoint.isPresent()) {
-            Endpoint endpoint = optionalEndpoint.get();
-            Optional<Application> optionalApplication = applicationService.findByEndpoint(endpoint);
+            final var endpoint = optionalEndpoint.get();
+            var optionalApplication = applicationService.findByEndpoint(endpoint);
             if (optionalApplication.isPresent()) {
-                Application application = optionalApplication.get();
+                final var application = optionalApplication.get();
                 model.addAttribute("endpoint", endpoint);
 
                 model.addAttribute("agrirouterApplication", application);
 
-                List<Warning> warnings = endpointService.getWarnings(endpoint);
+                final var warnings = endpointService.getWarnings(endpoint);
                 warnings.sort((o1, o2) -> Long.compare(o2.getTimestamp(), o1.getTimestamp()));
                 model.addAttribute("warnings", warnings);
 
-                List<Error> errors = endpointService.getErrors(endpoint);
+                final var errors = endpointService.getErrors(endpoint);
                 errors.sort((o1, o2) -> Long.compare(o2.getTimestamp(), o1.getTimestamp()));
                 model.addAttribute("errors", errors);
 
-                TechnicalConnectionState technicalConnectionState = mqttClientManagementService.getTechnicalState(application, endpoint.asOnboardingResponse());
+                final var technicalConnectionState = mqttClientManagementService.getTechnicalState(application, endpoint.asOnboardingResponse());
                 model.addAttribute("technicalConnectionState", technicalConnectionState);
 
                 model.addAttribute("connectionErrors", technicalConnectionState.connectionErrors());
 
                 model.addAttribute("cloudOnboardingFailures", cloudOnboardingFailureCache.getAll(endpoint.getExternalEndpointId()));
 
-                final var messagesWaitingForAcknowledgement = messageWaitingForAcknowledgementService.findAllForAgrirouterEndpointId(endpoint.getAgrirouterEndpointId())
+                final var messagesWaitingForAcknowledgement = new ArrayList<>(messageWaitingForAcknowledgementService.findAllForAgrirouterEndpointId(endpoint.getAgrirouterEndpointId())
                         .stream()
                         .map(messageWaitingForAcknowledgement -> modelMapper.map(messageWaitingForAcknowledgement, MessageWaitingForAcknowledgementDto.class))
-                        .peek(messageWaitingForAcknowledgementDto -> messageWaitingForAcknowledgementDto.setHumanReadableCreated(Date.from(Instant.ofEpochSecond(messageWaitingForAcknowledgementDto.getCreated())))).toList();
+                        .peek(messageWaitingForAcknowledgementDto -> messageWaitingForAcknowledgementDto.setHumanReadableCreated(Date.from(Instant.ofEpochSecond(messageWaitingForAcknowledgementDto.getCreated())))).toList());
+                messagesWaitingForAcknowledgement.sort((o1, o2) -> Long.compare(o2.getCreated(), o1.getCreated()));
                 model.addAttribute("messagesWaitingForAcknowledgement", messagesWaitingForAcknowledgement);
 
             } else {
