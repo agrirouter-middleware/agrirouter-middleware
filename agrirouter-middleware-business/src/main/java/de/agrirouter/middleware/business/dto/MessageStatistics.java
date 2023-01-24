68443d11eac8bf7de0663b031838f58676c6b931
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-01-24 11:14:27 +0100

    Feature/fetch message statistics for applications (#200)
    * Fetch message statistics for application.
    
    * Update OpenAPI documentation.

---
+++ agrirouter-middleware-business/src/main/java/de/agrirouter/middleware/business/dto/MessageStatistics.java
@@ -0,0 +1,90 @@
+package de.agrirouter.middleware.business.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * DTO for the message statistics.
+ */
+@Getter
+@Setter
+public class MessageStatistics {
+
+    private Map<String, MessageStatistic> messageStatistics = new HashMap<>();
+
+    /**
+     * Add a new message statistic to the list.
+     *
+     * @param senderId The sender ID.
+     * @param entry    The new entry for the message statistic.
+     */
+    public void addMessageStatisticEntry(String senderId, MessageStatistic.Entry entry) {
+        if (messageStatistics.containsKey(senderId)) {
+            messageStatistics.get(senderId).addEntry(entry);
+        } else {
+            messageStatistics.put(senderId, new MessageStatistic(entry));
+        }
+    }
+
+    /**
+     * DTO for the message statistic.
+     */
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class MessageStatistic {
+        /**
+         * Technical message type.
+         */
+        private int numberOfMessages;
+
+        /**
+         * The messages statistics grouped by technical message type.
+         */
+        private List<Entry> entries;
+
+        public MessageStatistic(Entry entry) {
+            this.numberOfMessages = entry.getNumberOfMessages();
+            this.entries.add(entry);
+        }
+
+        /**
+         * Add a new message statistic entry to the list.
+         *
+         * @param entry The message statistic entry.
+         */
+        public void addEntry(Entry entry) {
+            if (entries == null) {
+                numberOfMessages = entry.getNumberOfMessages();
+                entries = List.of(entry);
+            } else {
+                numberOfMessages += entry.getNumberOfMessages();
+                entries.add(entry);
+            }
+        }
+
+        /**
+         * DTO for the message statistic entry.
+         */
+        @Getter
+        @Setter
+        @AllArgsConstructor
+        public static class Entry {
+
+            /**
+             * Technical message type.
+             */
+            private String technicalMessageType;
+
+            /**
+             * Number of messages.
+             */
+            private int numberOfMessages;
+        }
+    }
+}
---
+++ agrirouter-middleware-controller/src/main/java/de/agrirouter/middleware/controller/dto/MessageStatisticsGroupedByApplicationResponse.java
@@ -0,0 +1,92 @@
+package de.agrirouter.middleware.controller.dto;
+
+import de.agrirouter.middleware.business.dto.MessageStatistics;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Response for the message statistics.
+ */
+@Getter
+@Setter
+@Schema(description = "Response for the message statistics.")
+public class MessageStatisticsGroupedByApplicationResponse {
+
+    private Map<String, List<MessageStatisticGroupedBySender>> messageStatistics = new HashMap<>();
+
+    /**
+     * Add a new message statistic for the endpoint to the list.
+     *
+     * @param applicationId                   The application ID.
+     * @param messageStatisticGroupedBySender The message statistics grouped by sender.
+     */
+    public void add(String applicationId, MessageStatisticGroupedBySender messageStatisticGroupedBySender) {
+        if (!messageStatistics.containsKey(applicationId)) {
+            messageStatistics.put(applicationId, List.of(messageStatisticGroupedBySender));
+        } else {
+            messageStatistics.get(applicationId).add(messageStatisticGroupedBySender);
+        }
+    }
+
+    /**
+     * The message statistics grouped by sender.
+     */
+    @Getter
+    @Setter
+    @Schema(description = "The message statistics grouped by sender.")
+    public static class MessageStatisticGroupedBySender {
+
+        /**
+         * The list of the message statistics grouped by sender.
+         */
+        @Schema(description = "The message statistics grouped by sender.")
+        private Map<String, MessageStatistics.MessageStatistic> messageStatistics;
+
+        /**
+         * DTO for the message statistic.
+         */
+        @Getter
+        @Setter
+        @Schema(description = "Single message statistic for the sender.")
+        public static class MessageStatistic {
+
+            /**
+             * Technical message type.
+             */
+            @Schema(description = "The overall number of messages.")
+            private int numberOfMessages;
+
+            /**
+             * The messages statistics grouped by technical message type.
+             */
+            @Schema(description = "The messages statistics grouped by technical message type.")
+            private List<Entry> entries;
+
+            /**
+             * DTO for the message statistic entry.
+             */
+            @Getter
+            @Setter
+            @Schema(description = "Single message statistic entry for a technical message type.")
+            public static class Entry {
+
+                /**
+                 * Technical message type.
+                 */
+                @Schema(description = "The technical message type.")
+                private String technicalMessageType;
+
+                /**
+                 * Number of messages.
+                 */
+                @Schema(description = "The number of messages.")
+                private int numberOfMessages;
+            }
+        }
+    }
+}
