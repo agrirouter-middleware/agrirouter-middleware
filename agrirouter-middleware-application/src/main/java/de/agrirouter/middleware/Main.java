3680e508384b25d110498282e8c7116adaf4b2c0
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-02-07 09:59:11 +0100

    Update OpenAPI description.

--- agrirouter-middleware-application/src/main/java/de/agrirouter/middleware/Main.java
+++ agrirouter-middleware-application/src/main/java/de/agrirouter/middleware/Main.java
@@ -1,5 +1,8 @@
 package de.agrirouter.middleware;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,6 +18,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 /**
  * Main entry of the application.
  */
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Agrirouter Middleware",
+                version = "5.0",
+                description = """
+                        The agrirouter© middleware was developed to have easier access to the functionality of the agrirouter.
+                        The agrirouter© middleware is an additional layer of abstraction and provides access to the agrirouter© without deep knowledge of the underlying processes.
+                        The agrirouter© middleware manages the connections and fetches messages from the agrirouter, based on the technical messages types registered.
+                        In addition, the agrirouter© middleware provides simple data conversion from ISOXML TaskData to EFDI Telemetry Data and provides searching operations for DDIs and other parts of the specification."""
+        ),
+        servers = @Server(
+                url = "n.a.",
+                description = "No public server available."
+        )
+)
 @EnableAsync
 @EnableScheduling
 @EnableWebSecurity
