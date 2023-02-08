da49a7562bdf8111a5cfb8e291447205712063b2
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-02-08 17:54:24 +0100

    Remove 'server' section from OpenAPI description. (#215)

--- agrirouter-middleware-application/src/main/java/de/agrirouter/middleware/Main.java
+++ agrirouter-middleware-application/src/main/java/de/agrirouter/middleware/Main.java
@@ -2,7 +2,6 @@ package de.agrirouter.middleware;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,10 +26,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
                         The agrirouter© middleware is an additional layer of abstraction and provides access to the agrirouter© without deep knowledge of the underlying processes.
                         The agrirouter© middleware manages the connections and fetches messages from the agrirouter, based on the technical messages types registered.
                         In addition, the agrirouter© middleware provides simple data conversion from ISOXML TaskData to EFDI Telemetry Data and provides searching operations for DDIs and other parts of the specification."""
-        ),
-        servers = @Server(
-                url = "n.a.",
-                description = "No public server available."
         )
 )
 @EnableAsync
