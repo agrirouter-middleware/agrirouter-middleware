a2d788416119b6c0df9e25fd584970c2355730ec
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-05-23 16:34:10 +0200

    Remove unused import.

--- agrirouter-middleware-business/src/main/java/de/agrirouter/middleware/business/TenantService.java
+++ agrirouter-middleware-business/src/main/java/de/agrirouter/middleware/business/TenantService.java
@@ -11,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
