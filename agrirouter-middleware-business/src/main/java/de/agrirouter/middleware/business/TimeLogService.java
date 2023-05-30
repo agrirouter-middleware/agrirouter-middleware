9e3a7799febdf62be44d3d956af3400c2c3b4600
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-05-30 13:07:33 +0200

    Bugfix/avoid npe in case data logs are not set (#301)

--- agrirouter-middleware-business/src/main/java/de/agrirouter/middleware/business/TimeLogService.java
+++ agrirouter-middleware-business/src/main/java/de/agrirouter/middleware/business/TimeLogService.java
@@ -185,8 +185,10 @@ public class TimeLogService {
                     final var timeEntries = document.getList("time", Document.class);
                     timeEntries.forEach(timeEntry -> {
                         final var dataLogValues = timeEntry.getList("dataLogValue", Document.class);
-                        final var filteredDataLogValues = dataLogValues.stream().filter(d -> messagesForTimeLogPeriodParameters.getDdisToList().contains(d.getInteger("processDataDdi"))).toList();
-                        timeEntry.put("dataLogValue", filteredDataLogValues);
+                        if (null != dataLogValues && !dataLogValues.isEmpty()) {
+                            final var filteredDataLogValues = dataLogValues.stream().filter(d -> messagesForTimeLogPeriodParameters.getDdisToList().contains(d.getInteger("processDataDdi"))).toList();
+                            timeEntry.put("dataLogValue", filteredDataLogValues);
+                        }
                     });
                     document.put("time", timeEntries);
                 });
