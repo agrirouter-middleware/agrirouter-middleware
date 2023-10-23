c25a8c38ea7ae6f5ec8ee30ef06095d7e43baf92
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-10-23 10:05:39 +0200

    "Added redirect delay feature for agrirouter
    Incorporated redirect delay into the agrirouter middleware in order to perform redirects smoothly. A new configuration parameter 'redirect-delay' was introduced in the application.yml file to hold the delay time, which is fetched via @Value annotation inside CallbackController.java. Additionally, the redirectDelay value is now passed as a query parameter in the redirect URL and used in the landing_page.html file to delay the redirect via JavaScript. This promotes greater control and customization of redirect behavior."

--- agrirouter-middleware-controller/src/main/java/de/agrirouter/middleware/controller/unsecured/CallbackController.java
+++ agrirouter-middleware-controller/src/main/java/de/agrirouter/middleware/controller/unsecured/CallbackController.java
@@ -6,6 +6,7 @@ import de.agrirouter.middleware.controller.UnsecuredApiController;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,6 +26,9 @@ import org.springframework.web.util.UriComponentsBuilder;
 )
 public class CallbackController implements UnsecuredApiController {
 
+    @Value("${app.agrirouter.redirect-delay:5000}")
+    private Long redirectDelay;
+
     /**
      * The landing page.
      *
@@ -41,6 +45,7 @@ public class CallbackController implements UnsecuredApiController {
                 .queryParam("token", token)
                 .queryParam("signature", signature)
                 .queryParam("error", error)
+                .queryParam("redirectDelay", redirectDelay)
                 .build().toUriString();
         return new RedirectView(redirectUrl);
     }
