451abad427efcac8b8d34f173062436af5a21b8b
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-10-24 08:41:56 +0200

    Feature/add messages that are more serious (#368)

--- agrirouter-middleware-application/src/main/java/de/agrirouter/middleware/controller/LandingPageController.java
+++ agrirouter-middleware-application/src/main/java/de/agrirouter/middleware/controller/LandingPageController.java
@@ -28,44 +28,7 @@ public class LandingPageController extends UIController {
         model.addAttribute("signature", signature);
         model.addAttribute("error", error);
         model.addAttribute("redirectUrl", Routes.UnsecuredEndpoints.CALLBACK_PROCESSOR);
-        model.addAttribute("loadingMessage", getRandomLoadingMessage());
         return Routes.UserInterface.ThymeleafRouting.LANDINGPAGE;
-    }
-
-    private String getRandomLoadingMessage() {
-        var messages = new String[]{
-                "Just a moment, we're revving up the engines for you.",
-                "Gathering stardust, almost there!",
-                "Hold tight, we're sculpting your experience.",
-                "Sipping digital coffee, almost brewed!",
-                "Tickling the servers, stay tuned!",
-                "In a parallel universe, your page is already loaded.",
-                "Summoning the digital spirits, stay patient.",
-                "Racing through cyberspace, just for you!",
-                "Building your digital paradise, brick by pixelated brick.",
-                "Just unfurling the digital red carpet for you.",
-                "Brace for impact! We're launching your experience.",
-                "Patience is a virtue. Your digital journey is about to begin.",
-                "Embrace the calm before the digital storm.",
-                "Brewing awesomeness... Just a moment.",
-                "Gathering stardust for your magical experience.",
-                "Loading the pixels of your dreams.",
-                "Revving up the engines of imagination.",
-                "Hold tight! Unleashing the magic in progress.",
-                "Whispering sweet nothings to our servers.",
-                "Embracing the infinite possibilities of cyberspace.",
-                "In the meantime, take a deep breath and smile.",
-                "Fueling up the digital rocket for your mission.",
-                "Loading... because even digital worlds need time to warm up.",
-                "The wheels of technology are in motion.",
-                "Unveiling the future, one pixel at a time.",
-                "Get ready to dive into a world of wonder.",
-                "Buckle up! Our servers are reaching warp speed.",
-                "Preparing a symphony of code and creativity.",
-                "One small step for you, one giant leap for your experience.",
-                "The countdown to digital brilliance has begun."
-        };
-        return messages[(int) (Math.random() * messages.length)];
     }
 
 }
