775ad3c6051e52500b964b683d59b00eeb4a7f91
Sascha Dömer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-10-31 18:31:22 +0100

    Switch to Jakarta for servlet imports
    Updated the servlet imports from `javax` to `jakarta` package to maintain compatibility with the latest Jakarta EE dependencies. This change ensures future-proofing of the application's servlet-handling capabilities.

--- agrirouter-middleware-application/src/main/java/de/agrirouter/middleware/config/RequestLoggingConfiguration.java
+++ agrirouter-middleware-application/src/main/java/de/agrirouter/middleware/config/RequestLoggingConfiguration.java
@@ -1,10 +1,9 @@
 package de.agrirouter.middleware.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * Logging the incoming requests.
