1ff61e5b9af674bf01d01fd51dedd6e1ec0b6c79
Sascha Dömer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2025-02-10 15:30:53 +0100

    Remove unnecessary @EnableAsync annotation from config
    The @EnableAsync annotation is redundant as it's not used or required in this configuration class. Its removal simplifies the code and avoids unnecessary imports or annotations.

--- agrirouter-middleware-application/src/main/java/de/agrirouter/middleware/config/AsyncConfiguration.java
+++ agrirouter-middleware-application/src/main/java/de/agrirouter/middleware/config/AsyncConfiguration.java
@@ -3,14 +3,12 @@ package de.agrirouter.middleware.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Slf4j
-@EnableAsync
 @Configuration
 public class AsyncConfiguration {
 
