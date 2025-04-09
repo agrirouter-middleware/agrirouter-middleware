bea205bdc0d1961c74dee80a7fd25fc5e79d6f14
Sascha Dömer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2025-04-09 21:19:33 +0200

    Add method to create and return message for acknowledgment.
    Refactored the logic to encapsulate the creation of `MessageWaitingForAcknowledgement` into a reusable method. This improves code readability and separation of concerns. Additionally, the new method ensures proper handling of `onboardingResponse` and `endpointDetailsParameters` while creating the acknowledgment message.

--- agrirouter-middleware-integration/src/main/java/de/agrirouter/middleware/integration/VirtualOnboardProcessIntegrationService.java
+++ agrirouter-middleware-integration/src/main/java/de/agrirouter/middleware/integration/VirtualOnboardProcessIntegrationService.java
@@ -1,5 +1,6 @@
 package de.agrirouter.middleware.integration;
 
+import com.dke.data.agrirouter.api.dto.onboard.OnboardingResponse;
 import com.dke.data.agrirouter.api.enums.SystemMessageType;
 import com.dke.data.agrirouter.api.service.parameters.CloudOnboardingParameters;
 import com.dke.data.agrirouter.impl.messaging.mqtt.CloudOnboardingServiceImpl;
@@ -11,6 +12,7 @@ import de.agrirouter.middleware.integration.container.VirtualEndpointOnboardStat
 import de.agrirouter.middleware.integration.mqtt.MqttClientManagementService;
 import de.agrirouter.middleware.integration.parameters.VirtualOnboardProcessIntegrationParameters;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -68,6 +70,11 @@ public class VirtualOnboardProcessIntegrationService {
         virtualEndpointOnboardStateContainer.push(messageId, endpointDetailsParameters.getEndpointId());
 
         log.debug("Saving message with ID '{}'  waiting for ACK.", messageId);
+        MessageWaitingForAcknowledgement messageWaitingForAcknowledgement = createMessageForAcknowledgement(onboardingResponse, messageId, endpointDetailsParameters);
+        messageWaitingForAcknowledgementService.save(messageWaitingForAcknowledgement);
+    }
+
+    private static @NotNull MessageWaitingForAcknowledgement createMessageForAcknowledgement(OnboardingResponse onboardingResponse, String messageId, CloudOnboardingParameters.EndpointDetailsParameters endpointDetailsParameters) {
         MessageWaitingForAcknowledgement messageWaitingForAcknowledgement = new MessageWaitingForAcknowledgement();
         messageWaitingForAcknowledgement.setAgrirouterEndpointId(onboardingResponse.getSensorAlternateId());
         messageWaitingForAcknowledgement.setMessageId(messageId);
@@ -75,6 +82,6 @@ public class VirtualOnboardProcessIntegrationService {
         final var dynamicProperties = new HashMap<String, Object>();
         dynamicProperties.put(EXTERNAL_VIRTUAL_ENDPOINT_ID, endpointDetailsParameters.getEndpointId());
         messageWaitingForAcknowledgement.setDynamicProperties(dynamicProperties);
-        messageWaitingForAcknowledgementService.save(messageWaitingForAcknowledgement);
+        return messageWaitingForAcknowledgement;
     }
 }
