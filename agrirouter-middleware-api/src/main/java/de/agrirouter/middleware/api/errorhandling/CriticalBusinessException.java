9041631bc8cc10fd4bf2e44b14378b3408ffaef0
Sascha Dömer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2025-04-09 21:55:53 +0200

    Remove unused CriticalBusinessException class.
    The CriticalBusinessException class has been deleted as it was no longer in use. This cleanup simplifies the codebase and ensures better maintainability.

--- agrirouter-middleware-api/src/main/java/de/agrirouter/middleware/api/errorhandling/CriticalBusinessException.java
+++
@@ -1,7 +0,0 @@
-package de.agrirouter.middleware.api.errorhandling;
-
-/**
- * Will be thrown in case of any critical business problem that has to be handled.
- */
-public class CriticalBusinessException extends Exception {
-}
