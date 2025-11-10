176b538a97dcbdc94ee01245d63207eb6e1d9853
Sascha Dömer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2025-11-10 16:56:47 +0100

    "Organize imports across controllers and domain classes to improve structure and readability."

--- agrirouter-middleware-domain/src/main/java/de/agrirouter/middleware/domain/ConnectionCriteria.java
+++ agrirouter-middleware-domain/src/main/java/de/agrirouter/middleware/domain/ConnectionCriteria.java
@@ -1,8 +1,8 @@
 package de.agrirouter.middleware.domain;
 
+import com.dke.data.agrirouter.api.dto.onboard.RouterDevice;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import com.dke.data.agrirouter.api.dto.onboard.RouterDevice;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
