6e809c23e166c3a7e047a3cb2a8f24f6e651d2d7
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2026-05-18 10:09:52 +0200

    Do not use the message of the exception or the exception itself.

--- agrirouter-middleware-business/src/main/java/de/agrirouter/middleware/business/listener/ReconnectAllOnboardResponsesEventListener.java
+++ agrirouter-middleware-business/src/main/java/de/agrirouter/middleware/business/listener/ReconnectAllOnboardResponsesEventListener.java
@@ -51,7 +51,7 @@ public class ReconnectAllOnboardResponsesEventListener {
                 }
             } catch (BusinessException e) {
                 if (ErrorKey.MISSING_ROUTER_DEVICE.equals(e.getErrorMessage().key())) {
-                    log.error("Could not reconnect a client, please check the client to avoid data loss. {}", e.getMessage());
+                    log.error("Could not reconnect a client, please check the client to avoid data loss.");
                 } else {
                     log.error("Could not reconnect a client, please check the client to avoid data loss.", e);
                 }
